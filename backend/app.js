27a15ffbd4237a17428fa6860f661d19d205d359
guymforer <guymforer@gmail.com>
2022-06-16 12:24:07 +0300

    first commit

---
+++ backend/app.js
@@ -0,0 +1,52 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+const cors = require('cors')
+require("dotenv").config();
+// import routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+
+
+// app
+const app = express();
+
+// db DO NOT CHANGE!!!
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "eshop-database",
+  })
+  .then(() => {
+    console.log("database is ready..");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
+
+// routes middleware
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
